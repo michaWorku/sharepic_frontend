05fddc5b80ec79c335884c7ea13548c250a57ce3
michaworku <mikias27worku@gmail.com>
2022-03-03 17:06:16 +0300

    Auth - Login Component using Google OAuth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login/Login'
+import Home from './Container/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/*' element={<Home />}/>
+    </Routes>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import {React} from 'React'
-import { ReactDOM } from 'react'
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+import {BrowserRouter as Router  } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+<Router>
+    <App/>
+</Router>, document.getElementById('root')
+)
