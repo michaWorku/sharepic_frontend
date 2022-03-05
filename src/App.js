776c91ebc551558d2c910bc19e87ec7c77f16272
michaworku <mikias27worku@gmail.com>
2022-03-05 13:53:17 +0300

    build issue fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import  { Login }  from './components/';
-import { Home } from './container/Home';
+import Home  from './container/Home';
 
 const App = () => {
   const navigate = useNavigate();
